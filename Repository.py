7ad138858871a4059338f22a330aaafc581a7d8a
koren12122 <koren121222@gmail.com>
2021-01-12 21:27:20 +0200

    basis

--- Repository.py
+++ Repository.py
@@ -19,3 +19,26 @@ class _Repository:
         self._conn.close()
 
 
+    def create_tables(self):
+        cursor = self._conn.cursor()
+        cursor.execute("""CREATE TABLE Vaccines(id INTEGER PRIMARY KEY,
+                                                    date DATE NOT NULL,
+                                                    supplier INTEGER REFERENCES Supplier(id),  
+                                                    coffee_stand INTEGER REFERENCES Coffee_stands(id))
+        """)
+
+        cursor.execute("""CREATE TABLE Suppliers(id INTEGER PRIMARY KEY,
+                                                          name STRING NOT NULL,
+                                                          logistic INTEGER REFERENCES Logistic(id))
+              """)
+        cursor.execute("""CREATE TABLE Clinics(id INTEGER PRIMARY KEY,
+                                                          location STRING NOT NULL,
+                                                          demand INTEGER NOT NULL,
+                                                          logistic INTEGER REFERENCES logistic(id))
+              """)
+        cursor.execute("""CREATE TABLE Logistics(id INTEGER PRIMARY KEY,
+                                                              name STRING NOT NULL,
+                                                              count_sent INTEGER NOT NULL,
+                                                              count_received INTEGER NOT NULL)
+              """)
+
