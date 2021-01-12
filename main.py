bc4b16d2d6814770e77e136c26d6422a37ce694e
royamoyal <roy15957@gmail.com>
2021-01-13 00:04:37 +0200

    order_report done2

--- main.py
+++ main.py
@@ -1,5 +1,3 @@
-
-
 import sys
 
 from Repository import *
@@ -18,23 +16,19 @@ def main(config_file, order_file, output_file):
         order_lines = o.readlines()
     repo.order_active(order_lines)
 
+    # this method will be in repository
+    def order_active(self, order_lines):
+        for line in order_lines:
+            args = line.split(',')  # we need to make sure that the split not ruin the line
+            args = [arg.strip() for arg in args]
+            if len(args) == 3:
+                repo.recieve_shipment(line)
+            else:
+                repo.send_shipment(line)
+
 
 if __name__ == "__main__":
     if len(sys.argv) != 4:
         raise ValueError("usage - main.py config.txt order.txt output.txt")
     main(sys.argv[1], sys.argv[2], sys.argv[3])
-
-
-    # this method will be in repository
-    def order_active(self, order_lines):
-        for line in order_lines:
-            args = line.split(',')    # we need to make sure that the split not ruin the line
-            args = [arg.strip() for arg in args]
-            if len(args) == 3 :
-                recive_shipment(line)
-            else:
-                send_shipment(line)
-
-
-
 
